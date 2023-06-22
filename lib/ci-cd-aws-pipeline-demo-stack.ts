86a7ee0609d98d426a21626d9441644dcc6582ba
Giorgi Kochakidze <giorgi.kochakidze@deeto.ai>
2023-06-21 21:46:15 -0400

    initial commit

---
+++ lib/ci-cd-aws-pipeline-demo-stack.ts
@@ -0,0 +1,21 @@
+import * as cdk from 'aws-cdk-lib';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import { Construct } from 'constructs';
+
+export class CiCdAwsPipelineDemoStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const pipeline = new CodePipeline(this, 'Pipeline', {
+      pipelineName: 'TestPipeline',
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('gkochakidze-deeto/ci-cd-aws-pipeline-demo', 'main'),
+        commands: [
+          'npm ci',
+          'npm run build',
+          'npx cdk synth'
+        ]
+      })
+    })
+  }
+}
